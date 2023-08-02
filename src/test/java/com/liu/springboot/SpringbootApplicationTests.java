03cfd2f9a683a714cc579b82055905d6b02197ad
LiuHaoYu <liu030910@gmail.com>
2023-08-02 09:32:04 +0800

    测试类测试通过

--- src/test/java/com/liu/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/liu/springboot/SpringbootApplicationTests.java
@@ -1,13 +1,23 @@
 package com.liu.springboot;
 
+import com.liu.springboot.mapper.AddressMapper;
+import com.liu.springboot.pojo.Address;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @SpringBootTest
 class SpringbootApplicationTests {
 
+    @Resource
+    AddressMapper addressMapper;
+
     @Test
     void contextLoads() {
+        List<Address> list = addressMapper.selectList(null);
+        list.forEach(System.out::println);
     }
 
 }
